063dcc9b689a4a6f3e50c47a5fa70aea2a057ce3
Felipe Friedrich Santos <54379678+FelipeFriedrich@users.noreply.github.com>
2021-11-16 00:38:21 -0300

    Refatoração da tabela campanha

--- src/database/models/campanhas.js
+++ src/database/models/campanhas.js
@@ -10,25 +10,26 @@ const Campanhas = sequelize.define("campanhas", {
       allowNull: false,        
     },
     nomeDaCampanha: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(50),
       allowNull: false,
+      unique: true,
     },
     primeiroCupom: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(30),
       allowNull: false,
     },
     segundoCupom:{
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(30),
     },
     terceiroCupom:{
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(30),
     },
     tituloEmail :{
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(50),
         allowNull: false,
       },
     textoEmail :{
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(3000),
         allowNull: false,
     },
   });
